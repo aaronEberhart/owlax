abd2e351fa6b52908091132edd9351d37f5b718b
sulogna-chy <61206614+sulogna-chy@users.noreply.github.com>
2020-03-09 09:59:08 -0500

    Update OWLAxEvaluation.java
    edited evaluation

--- src/main/java/evaluation/OWLAxEvaluation.java
+++ src/main/java/evaluation/OWLAxEvaluation.java
@@ -17,48 +17,29 @@ public class OWLAxEvaluation {
 	float totalAxioms;
 	float totalclasses;
 	float average_total;
+	double array_size;
+	HashMap<String, Integer> val ;
+	HashMap<String, Integer> val_ont;
 	
 	public OWLAxEvaluation(ArrayList<ArrayList<HashMap<String,Integer>>> resultsList) {
 		
 		// results are lists of 2 things: ontology stats and OWLAx evaluations
 		allResults = resultsList;
-		//System.out.println(allResults);
-				
+		
 		//split the results list into ontology and owlax data
 		owlaxResults = new ArrayList<HashMap<String,Integer>>();
 		
 		ontologyCompositions = new ArrayList<HashMap<String,Integer>>();
 		allResults.forEach(a -> {owlaxResults.add(a.get(0));ontologyCompositions.add(a.get(1));});
-		
-		HashMap<String, Integer> val = owlaxResults.get(0);
-		//total axioms in oxlax
-		 totalAxioms = 0.0f;
-		for (float f : val.values()) {
-			totalAxioms += f;
-		}
-	    
-		ArrayList<HashMap<String, Integer>> ontology_result = ontologyCompositions ;
-		HashMap<String, Integer> val_ont = ontology_result.get(0);
-		// total classes in ontology
-		 totalclasses = 0.0f;
-		for (float f : val_ont.values()) {
-			totalclasses += f;
-		}
 
-		//average 
-	   average_total = totalAxioms / totalclasses;
 		
-//		System.out.println(totalAxioms);
-//		System.out.println( totalclasses);
-//		System.out.println(average_total);
-//		
-		// calculating the averages
+	   // calculating the averages
 		averageResult = (HashMap<String,Double>)owlaxResults.stream().flatMap(hashMap -> hashMap.entrySet().stream()).collect(Collectors.groupingBy(Map.Entry::getKey,Collectors.averagingInt(Map.Entry::getValue)));		
 		averageOntology = (HashMap<String,Double>)ontologyCompositions.stream().flatMap(hashMap -> hashMap.entrySet().stream()).collect(Collectors.groupingBy(Map.Entry::getKey,Collectors.averagingInt(Map.Entry::getValue)));
 		//removes the labels from the average ontology
 	    averageOntology.values().removeIf(a -> a < 0);
 	    
-	    //System.out.println(averageResult);
+	    
 	    // average coverage for average result ignoring other
 	    totalCoverage = averageResult.values().stream().reduce(0.0,(a,b) -> (averageResult.get("other") == a || averageResult.get("other") == b) ? 0 : a+b) / allResults.size();
 	    
@@ -89,11 +70,40 @@ public class OWLAxEvaluation {
 		StringBuilder sb = new StringBuilder();
 		sb.append(String.format("Evaluation Results:\n\n"));
 		//TODO
-		sb.append(String.format("Total Owlax matched axioms: " +totalAxioms ));
-		sb.append(String.format("\n\nTotal classes :- " + totalclasses));
-		sb.append(String.format("\n\nAverage :- " + average_total));
+		sb.append(String.format(getvalue()));
 		return sb.toString();
 	}
 
+	public String getvalue() {
+		array_size = owlaxResults.size() ;
+		StringBuilder newval = new StringBuilder();
+		for (int i = 0; i <= array_size-1; i = i + 1) {
+			  val = owlaxResults.get(i);
+			
+			   //total axioms in oxlax
+			   totalAxioms = 0.0f;
+			  for (float f : val.values()) {
+				totalAxioms += f;
+			  }
+		    
+			  ArrayList<HashMap<String, Integer>> ontology_result = ontologyCompositions ;
+			  val_ont = ontology_result.get(i);
+			  
+			  // total classes in ontology
+			  totalclasses = 0.0f;
+			  for (float f : val_ont.values()) {
+				totalclasses += f;
+			  }
+
+			  //average 
+		      average_total = totalAxioms / totalclasses;
+		      
+		      newval.append(String.format("\n\nTotal Owlax matched axioms: " + totalAxioms ));
+		      newval.append(String.format("\n\nTotal classes :- " + totalclasses));
+		      newval.append(String.format("\n\nAverage :- " + average_total));
+		     
+		     }
+		 return newval.toString() ;
+		}
 	
 }
